b3f9b274da06f8d2a1981689663d73a1884ba7d6
LeFrommage <bryan.fouda@student.hogent.be>
2026-02-23 11:50:40 +0100

    Mongo updates

--- FakeWebShop.Api/Program.cs
+++ FakeWebShop.Api/Program.cs
@@ -1,3 +1,5 @@
+using FakeWebShop.Domain.Services;
+using FakeWebShop.Domain.Services.MongoInterfaces;
 using FakeWebShop.Persistence.MongoRepo_s;
 using FakeWebShop.Persistence.MongoRepo_s.MongoInterface_s;
 using FakeWebShop.Persistence.MongoRepo_s.Options;
@@ -5,16 +7,17 @@ using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Bind MongoDb Options
-builder.Services.Configure<MongoOptions>(
-    builder.Configuration.GetSection("Mongo"));
 
-// builder.Services.AddSingleton<IMongoClient>(_ =>
-//     new MongoClient(builder.Configuration["Mongo:ConnectionString"]));
+// MongoClient registeren (Singleton)
+builder.Services.AddSingleton<IMongoClient>(_ =>
+    new MongoClient(builder.Configuration["Mongo:ConnectionString"]));
 
-
-// Dependency Injection
+// Repository DI
 builder.Services.AddScoped<IMongoProductRepository, MongoProductRepository>();
+
+// Service DI
+builder.Services.AddScoped<IMongoProductService, MongoProductService>();
+
 
 var app = builder.Build();
 
