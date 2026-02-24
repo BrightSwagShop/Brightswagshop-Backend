274494a0ac69f47439e911c8f86ad8af135ab5e7
soulinee <91409289+soulinee@users.noreply.github.com>
2026-02-24 15:37:38 +0100

    Program en Controllers aangemaakt

--- FakeWebShop.Api/Program.cs
+++ FakeWebShop.Api/Program.cs
@@ -1,21 +1,42 @@
+using FakeWebShop.Persistence;
+using FakeWebShop.Persistence.repos;
+using FakeWebShop.Domain.Services;
+using FakeWebShop.Domain.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using FakeWebShop.Domain.Services.services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Controllers + Swagger
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
  
+
+// DbContext (liefst via appsettings, maar dit werkt nu direct)
+var cs = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ProductDbContext>(options =>
+    options.UseMySql(cs, ServerVersion.AutoDetect(cs)));
+
+// Repositories
+builder.Services.AddScoped<ICategoryRepo, CategoryRepo>();
+builder.Services.AddScoped<IProductenRepo, ProductenRepo>();
+// services
+builder.Services.AddScoped<IProductService, ProductService>();
+
+// repos
+builder.Services.AddScoped<IProductsRepo, ProductsRepo>();
+// Services
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IProductenService, ProductenService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+ 
 
 app.UseHttpsRedirection();
 
- 
- 
+app.MapControllers();
 
 app.Run();
- 
