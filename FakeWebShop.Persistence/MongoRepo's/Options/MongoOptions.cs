d67035e1a4df34f36c60e581ca3419d3219cb832
LeFrommage <bryan.fouda@student.hogent.be>
2026-03-12 11:13:52 +0100

    Image endpoint & Supabase storage toevoegen + porducttypes endpoint + Cors toevoegen

--- FakeWebShop.Persistence/MongoRepo's/Options/MongoOptions.cs
+++ FakeWebShop.Persistence/MongoRepo's/Options/MongoOptions.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace FakeWebShop.Persistence.MongoRepo_s.Options;
 
-public class MongoOptions
+public sealed class MongoOptions
 {
     public string ConnectionString { get; set; } = default!;
     public string Database { get; set; } = default!;
