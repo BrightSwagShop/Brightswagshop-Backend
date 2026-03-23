a62c336b8cb85c33caf9f75fe713f8dd07f533ae
soulinee <91409289+soulinee@users.noreply.github.com>
2026-03-23 17:00:50 +0100

    Implementatie publieke user toegevoegd

---
+++ FakeWebShop.Api/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+using FakeWebShop.Contracts.UserContracts;
+using FakeWebShop.Domain.Services.MongoUserServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FakeWebShop.Api.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UserController(MongoUserService service) : ControllerBase
+    {
+        [HttpPost("register")]
+    public async Task<ActionResult<UserResponseContract>> Register([FromBody] UserRequestContract request)
+    {
+        var user = await service.Register(request);
+        return Ok(user);
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<UserResponseContract>> Login([FromBody] UserRequestContract request)
+    {
+        var user = await service.Login(request);
+        if (user == null)
+            return Unauthorized();
+
+        return Ok(user);
+    }
+        
+    }
+}
